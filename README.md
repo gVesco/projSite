a0133fc49ca9c4fceb6ab2101fa3eb86123621f4
Guilherme Vesco <gui.vesco@hotmail.com>
2021-05-18 16:21:09 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Teste de leia-me.
---
+++ script.js
@@ -0,0 +1,24 @@
+document.write("Textando");
+//alert("Olá mundo!");
+console.log("mensagem exibida no console");
+
+var x = 10;
+var y = 5;
+var total = x + y;
+//alert ("A somal total é igual a " + total + ".");
+
+var hora = 18;
+
+if (hora == 12 || hora == 18) {
+	console.log("HORA DO RUSH.");
+} else {
+	console.log("Horário comum.");
+}
+
+/*
+var nomecompleto = nome + sobrenome;
+alert ("Seu nome completo é " + nomecompleto + ".")
+*/
+document.getElementById('hOne').innerHTML = 'Ronaldo <button>Benjamin Botão</button>';
+
+document.getElementsByClassName('lista')[0].innerHTML = 'Item alterado!';
