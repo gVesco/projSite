9daa42cb9e6e65ccb67d0aa5448f3c532ef8a276
Guilherme A. Vesco <guilhermeamaro@madeiranit.com.br>
2021-09-20 08:13:20 -0300

    Projeto teste inicial

--- script.js
+++ script.js
@@ -16,32 +16,19 @@ function azul () {
 	document.querySelector('#exemplo').classList.add('azul')
 }
 
-
-
-
-/*
-document.write("Textando");
-//alert("Olá mundo!");
-console.log("mensagem exibida no console");
-
-var x = 10;
-var y = 5;
-var total = x + y;
-//alert ("A somal total é igual a " + total + ".");
-
-var hora = 18;
-
-if (hora == 12 || hora == 18) {
-	console.log("HORA DO RUSH.");
-} else {
-	console.log("Horário comum.");
+function trocar() {
+	if (document.querySelector('#exemplo').classList.contains('verde')) {
+		document.querySelector('#exemplo').classList.remove('verde');
+		document.querySelector('#exemplo').classList.add('vermelho');
+	}
+	else if (document.querySelector('#exemplo').classList.contains('vermelho'))
+	{
+		document.querySelector('#exemplo').classList.remove('vermelho');
+		document.querySelector('#exemplo').classList.add('azul');
+	}
+	else
+	{
+		document.querySelector('#exemplo').classList.remove('azul');
+		document.querySelector('#exemplo').classList.add('verde');
+	}
 }
-
-
-var nomecompleto = nome + sobrenome;
-alert ("Seu nome completo é " + nomecompleto + ".")
-
-document.getElementById('hOne').innerHTML = 'Ronaldo <button>Benjamin Botão</button>';
-
-document.getElementsByClassName('lista')[0].innerHTML = 'Item alterado!';
-*/
